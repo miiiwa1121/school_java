7df4f1903309f0374737005b6e1c21fc6027c020
miwa <k023c0026@g.neec.ac.jp>
2024-11-14 15:30:40 +0900

    ok

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {//先頭大文字がスタンダード
+    public static void main(String[] argv){
+        System.out.println("Hello World");
+    }
+}
+//static...静的
+//void...戻り値がない
+//main...メソッド名
+//javac App.java => App.class...コンパイルしている
+//java App...実行
